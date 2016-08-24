d9084e60eb2af078fc60e89aef0d74bef093551b
Anulfo <noaharkling@gmail.com>
2016-08-24 05:14:40 -0500

    playing with the Ajax

--- songs.js
+++ songs.js
@@ -1,34 +1,20 @@
-var songs = [];
+$(document).ready(function() {
 
-songs[songs.length] = "Legs > by Z*ZTop on the album Eliminator";
-songs[songs.length] = "The Logical Song > by Supertr@amp on the album Breakfast in America";
-songs[songs.length] = "Another Brick in the Wall > by Pink Floyd on the album The Wall";
-songs[songs.length] = "Welco(me to the Jungle > by Guns & Roses on the album Appetite for Destruction";
-songs[songs.length] = "Ironi!c > by Alanis Moris*ette on the album Jagged Little Pill"
+  $.ajax({
+    url: "songsFile.json"
+  }).done(function(contentsOfTheFile) {
+    // When you tell jQuery to read a file via the ajax method
+    // it reads the contents, and then executes whatever function
+    // that you specify here in the done() method, and passes in
+    // the contents of the file as the first argument.
+    console.log("the contents of songsFile.json");
+    populateDom(contentsOfTheFile);
+  });
 
-// The Grinder
-var cleanStrings = []
-for (var i = 0; i < songs.length; i++) {
-cleanStrings[i] = songs[i].replace(">", "-");
-cleanStrings[i] = cleanStrings[i].replace("*", "");
-cleanStrings[i] = cleanStrings[i].replace("@", "");
-cleanStrings[i] = cleanStrings[i].replace("!", "");
-cleanStrings[i] = cleanStrings[i].replace("(", "");
-cleanStrings.push(cleanStrings[i]);
+var populateDom = function (contentsOfTheFile) {
+    console.log(contentsOfTheFile);
+    console.log(contentsOfTheFile.songs.breakfast);
 }
-console.log(cleanStrings);
-var newSong = "Knives Out by Radiohead on the album Amnesiac";
-var newSong2 = "Color a Esperanza by Diego Torres on the album MTV Unplugged";
-cleanStrings.push(newSong);
-cleanStrings.unshift(newSong2);
-console.log(cleanStrings);
-var theDom = document.getElementById("songList");
-
-// try for each later 
-for (var i = 0; i < cleanStrings.length; i++) {
-theDom.innerHTML += "<li>" + cleanStrings[i] + "</li>";
-}
-
 $("#addView").click(function() {
     console.log("HI")
     $(".listMusicView").addClass("hidden");
@@ -40,3 +26,5 @@ $("#listView").click(function() {
     $(".addMusicView").addClass("hidden");
     $(".listMusicView").removeClass("hidden");
 })
+
+});
